9443de78b8ac425624789acb7c4e64752858d165
Radha <parmar.radha8195@gmail.com>
2024-03-04 16:11:51 -0500

    first modified files

--- src/main/java/MavenTestFramework/App.java
+++ src/main/java/MavenTestFramework/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.print(false);
+        System.out.println(123);
+        System.out.println("Radha");
     }
 }
